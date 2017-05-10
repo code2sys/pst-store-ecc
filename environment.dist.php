1a15bc8e278fbdd00e8b8927c7cb415a4dfcab8c
Jonathan Brown <jonathan@resultsathand.com>
2017-05-10 09:49:47 -0400

    Add these to the environment

--- environment.dist.php
+++ environment.dist.php
@@ -17,3 +17,11 @@ define('STORE_DIRECTORY', __DIR__);
 define('SEARCH_NOINDEX', true);
 define('CORE_DIRECTORY', '');
 define('EBAY_TOKEN', '');
+
+// These are top level category numbers.
+// They were factored out of the code. The defaults are probably correct except for VTwin
+define('TOP_LEVEL_CAT_STREET_BIKES', 20409);
+define('TOP_LEVEL_CAT_ATV_PARTS', 20419);
+define('TOP_LEVEL_CAT_UTV_PARTS', 20422);
+define('TOP_LEVEL_CAT_VTWIN_PARTS', 69597);
+define('TOP_LEVEL_CAT_DIRT_BIKES', 20416);
