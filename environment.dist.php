a7c734c8c85677dde28356d703bff14c383148f4
Jonathan Brown <jonathan@resultsathand.com>
2017-08-07 10:24:01 -0400

    Here

--- environment.dist.php
+++ environment.dist.php
@@ -17,6 +17,7 @@ define('STORE_DIRECTORY', __DIR__);
 define('SEARCH_NOINDEX', true);
 define('CORE_DIRECTORY', '');
 define('EBAY_TOKEN', '');
+define('STORE_BANNER_LIBRARY', __DIR__.'/html/bannerlibrary');
 
 // These are top level category numbers.
 // They were factored out of the code. The defaults are probably correct except for VTwin
@@ -34,3 +35,16 @@ define('TOP_LEVEL_PAGE_ID_STREET', 3);
 define('TOP_LEVEL_PAGE_ID_UTV', 4);
 define('TOP_LEVEL_PAGE_ID_HOME', 0);
 
+
+// This disables all the motorcycle functionality and instead it just reverts to a parts store.
+define('MOTORCYCLE_SHOP_DISABLE', false);
+
+// this defaults to 8 if not set otherwise
+define('FOOTER_PAGE_LIMIT', 8);
+
+// this lets you disable the whole front navigation
+define('DISABLE_FRONT_MOTORCYCLE_NAV', false);
+define('MOTORCYCLE_SHOP_USED', true);
+
+// this lets you disable this button, or not.
+define('ENABLE_OEMPARTS_BUTTON', false);
