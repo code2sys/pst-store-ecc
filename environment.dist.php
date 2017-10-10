9b5f46a468ccd36e6faa5daeb22408c540e5b9a7
Jonathan Brown <jonathan@resultsathand.com>
2017-10-10 10:17:23 -0400

    Adding in the placeholder numbers for pst/core#19

--- environment.dist.php
+++ environment.dist.php
@@ -26,6 +26,7 @@ define('TOP_LEVEL_CAT_ATV_PARTS', 20419);
 define('TOP_LEVEL_CAT_UTV_PARTS', 20422);
 define('TOP_LEVEL_CAT_VTWIN_PARTS', 69597);
 define('TOP_LEVEL_CAT_DIRT_BIKES', 20416);
+define('TOP_LEVEL_CAT_MARINE', 99999);
 
 // These are page IDs. They are referenced at the top of the controllers.
 define('TOP_LEVEL_PAGE_ID_VTWIN', 17);
@@ -34,6 +35,7 @@ define('TOP_LEVEL_PAGE_ID_DIRT', 1);
 define('TOP_LEVEL_PAGE_ID_STREET', 3);
 define('TOP_LEVEL_PAGE_ID_UTV', 4);
 define('TOP_LEVEL_PAGE_ID_HOME', 0);
+define('TOP_LEVEL_PAGE_ID_MARINE', 0);
 
 
 // This disables all the motorcycle functionality and instead it just reverts to a parts store.
